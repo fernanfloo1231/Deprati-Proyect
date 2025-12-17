f9fcd6c2084093d47fec98e2aaa690a6a99ffbb0
Bryan Gonzalez <joffrearturocartuchejimenez@gmail.com>
2025-12-17 03:21:06 -0500

    Configura conexión a base de datos para producción

--- back-end/src/config/db.js
+++ back-end/src/config/db.js
@@ -2,18 +2,19 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-    host: process.env.PG_HOST,
-    port: process.env.PG_PORT,
-    user: process.env.PG_USER,
-    password: process.env.PG_PASSWORD,
-    database: process.env.PG_DATABASE,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 pool.connect()
   .then(client => {
-    console.log(' Conexión exitosa a la base de datos PostgreSQL');
+    console.log('✅ Conectado a PostgreSQL');
     client.release();
   })
-  .catch(err => console.error('❌ Error al conectar a la base de datos:', err));
+  .catch(err => {
+    console.error('❌ Error PostgreSQL:', err);
+  });
 
 module.exports = pool;
